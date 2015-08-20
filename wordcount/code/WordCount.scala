d6d7ebe75e46def54ed632420840f5c5e98645ab
felipe <falmeida1988@gmail.com>
2015-08-20 02:18:57 -0300

    spark stuff... not working yet... failed for some reason lets find out why

---
+++ wordcount/code/WordCount.scala
@@ -0,0 +1,19 @@
+import scala.math.random
+import org.apache.spark._
+object WordCount{
+	def main(args:Array[String]){
+
+		val input_dir = "s3://bdnc2/input"
+		val output_dir = "s3://bdnc2/output_spark"	
+
+		val cnf      = new SparkConf().setAppName("Spark WordCount")
+		val sc       = new SparkContext(cnf)
+	    val textFile = sc.textFile(input_dir)
+		val counts   = textFile.flatMap(line => line.split("\\+")) 
+                       .map(word => (word, 1)) 
+                       .reduceByKey( (a,b) => a+b )
+
+		counts.saveAsTextFile(output_dir)
+		sc.stop()
+	}
+}
---
+++ wordcount/code/spark/build.sbt
@@ -0,0 +1,9 @@
+name := "WordCount"
+
+version := "1.0"
+
+scalaVersion := "2.10.4"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.4.1"
+
+resolvers += "Akka Repository" at "http://repo.akka.io/releases/"
