f897d3405ab355829f96e4e4772f626aa0ffbed9
felipe <falmeida1988@gmail.com>
2015-08-26 02:16:08 -0300

    lotsa work done..

--- wordcount/code/spark/src/main/scala/WordCount.scala
+++ wordcount/code/spark/src/main/scala/WordCount.scala
@@ -6,17 +6,38 @@ object WordCount{
 	
 	def main(args:Array[String]){
 
-		val input_dir = "s3://bdnc2/input"
-		val output_dir = "s3://bdnc2/output_spark"	
+		if(args.length < 1){
+			System.err.println("Please set arguments for <s3_input_dir> <s3_output_dir>")
+			System.exit(1)
+		}
 
-		val cnf      = new SparkConf().setAppName("Spark WordCount")
+		val startNano = System.nanoTime
+
+		val inputDir = args(0)
+		val outputDir = args(1)	
+
+		val cnf      = new SparkConf().setAppName("Spark Distributed WordCount")
 		val sc       = new SparkContext(cnf)
-	    val textFile = sc.textFile(input_dir)
+		
+	    val textFile = sc.textFile(inputDir)
 		val counts   = textFile.flatMap(line => line.split("\\s+")) 
                        .map(word => (word, 1)) 
                        .reduceByKey( (a,b) => a+b )
 
-		counts.saveAsTextFile(output_dir)
+		counts.saveAsTextFile(outputDir)	
 		sc.stop()
+		
+		val endNano = System.nanoTime - startNano
+		val endSeconds = endNano / 10e-9
+
+		System.out.println("\n\n\n")
+		System.out.println("========================================")	
+		System.out.println("========================================")
+		System.out.println(s"Total time elapsed: $endSeconds seconds")
+		System.out.println("========================================")	
+		System.out.println("========================================")
+		System.out.println("\n\n\n") 
+		
+
 	}
 }
